caced7cdb139589eae2769c5f9ce5b845060866e
Abdur-Rahman-s <arsw5649@gmail.com>
2024-11-13 21:50:40 +0600

    Responsive Dynamic Navbar in this Degic App

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiAlignRight } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
 import logo from '../../public/Logo.png';
 import Button from '@mui/material/Button';
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false);
+    function togglelMenu() {
+        setOpen((prevOpen) => !prevOpen)
+    }
     const navItems = ["About", "Services", "Projects", "Contacts"];
-    
     return (
-        <header className=' py-3 border border-b-[#00000] shadow-[0px 4px 4px 0px rgba(0, 0, 0, 0.25)] '  >
-            <div className="container mx-auto overflow-hidden flex justify-between items-center">
-                {/* Logo Section */}
-                <div>
-                    <a href="#"><img src={logo} alt="Logo" className="max-h-36" /></a>
+        <header >
+            <div className='px-4 lg:px-0 py-3 text-[#000] border border-b-[#00000] shadow-[0px 4px 4px 0px rgba(0, 0, 0, 0.25)] '   >
+                <div className="container mx-auto overflow-hidden flex justify-between items-center">
+                    {/* Logo Section */}
+                    <div>
+                        <a href="#"><img src={logo} alt="Logo" className="max-h-36" /></a>
+                    </div>
+
+                    {/* Navbar Menu */}
+                    <nav className='flex gap-[29px] items-center ' >
+                        <ul className="sm:flex gap-8 text-gray-700 hidden ">
+                            {navItems.map((item) => (
+                                <li key={item} className="hover:text-[#2AB691] transition-colors duration-300">
+                                    <a href="#">{item}</a>
+                                </li>
+                            ))}
+                        </ul>
+
+                        {/* mobileMenu icon  */}
+                        <div className='block sm:hidden ' onClick={togglelMenu} >
+                            {open ? <RxCross1 /> : <FiAlignRight />}
+                        </div>
+                        {/* singUp Button  */}
+                        <div>
+                            <Button variant="outlined" style={{ backgroundColor: '#2AB6911A', color: '#2AB691', border: '1px solid  #2AB691' }} >Sign Up</Button>
+                        </div>
+                    </nav>
                 </div>
-                
-                {/* Navbar Menu */}
-                <nav className='flex gap-[29px] items-center ' >
-                    <ul className="flex gap-8 text-gray-700">
+            </div>
+            {/* mobilemenu  */}
+            {open && (
+                <div className="sm:hidden bg-[#2AB6911A] w-1/2 animate-slideInRight fixed top-20 right-0 h-full">
+                    <ul className="flex flex-col items-start ml-5 font-bold gap-4 py-4 text-gray-700">
                         {navItems.map((item) => (
                             <li key={item} className="hover:text-[#2AB691] transition-colors duration-300">
                                 <a href="#">{item}</a>
                             </li>
                         ))}
                     </ul>
-                    <div>
-                    <Button variant="outlined" style={{backgroundColor:'#2AB6911A' , color:'#2AB691' , border:'1px solid  #2AB691' }} >Sign Up</Button>
                 </div>
-                </nav>
+            )}
 
-            </div>
         </header>
     );
 }
